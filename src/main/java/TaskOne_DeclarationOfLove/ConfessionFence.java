2542e0b5860697212c2f70f975bbe6a1ae51a8cc
StepsonOfTheWinds <basilfisher@mail.ru>
2024-02-27 17:08:34 +0300

    "The_Test" commit 3: task 1 is completed. Perhaps this program will be improved in the future.

--- src/main/java/TaskOne_DeclarationOfLove/ConfessionFence.java
+++ src/main/java/TaskOne_DeclarationOfLove/ConfessionFence.java
@@ -13,18 +13,20 @@ public class ConfessionFence {
     int numOfLetters = 15;
     int numOfSpaces = 3;
     int fenceLength;
-
     int calculationResult;
+    boolean correctCommand = true;
 
     public void setFenceLength() {
         Scanner inFenceLength = new Scanner(System.in);
+        Scanner finishIt = new Scanner(System.in);
         int fl;
+        String end;
         System.out.println("Введите длину забора.");
         if (inFenceLength.hasNextInt()) {
             fl = inFenceLength.nextInt();
             fenceLength = fl;
-        } else {
-            System.err.println("Введите число c помощью цифр на клавиатуре!");
+        } else  {
+            correctCommand = false;
         }
     }
 
@@ -32,19 +34,24 @@ public class ConfessionFence {
         int allLetterLength = (numOfLetters/3) * 62;
         int allSpaseLength = numOfSpaces * 12;
         calculationResult = allLetterLength + allSpaseLength;
-        if ((calculationResult) <= fenceLength) {
-            System.out.println("Для надписи нужно " + calculationResult + " см.");
-            System.out.println("Вашей длины в " + fenceLength + " см достаточно для признания!");
-        } else {
-            int diff = calculationResult - fenceLength;
-            System.out.println("Заявленной длины забора недостаточно. Нужно ещё " + diff + " см.");
-            System.out.println("Придётся поискать забор помасштабнее...");
+        if(correctCommand) {
+            if ((calculationResult) <= fenceLength) {
+                System.out.println("Для надписи нужно " + calculationResult + " см.");
+                System.out.println("Вашей длины в " + fenceLength + " см достаточно для признания!");
+            } else {
+                int diff = calculationResult - fenceLength;
+                System.out.println("Заявленной длины забора недостаточно. Нужно ещё " + diff + " см.");
+                System.out.println("Придётся поискать забор помасштабнее...");
+            }
+        }
+        if(!correctCommand) {
+            System.err.println("Введите число c помощью цифр на клавиатуре!");
         }
     }
 
     public static void main(String[] args) {
         ConfessionFence FirstLove = new ConfessionFence();
         FirstLove.setFenceLength();
-            FirstLove.mainCalculation(FirstLove.fenceLength);
+        FirstLove.mainCalculation(FirstLove.fenceLength);
     }
 }
